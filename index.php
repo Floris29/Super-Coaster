4a23164f5a8d768b207bf09de4dfc6ac3789c9a6
tijn-avontuur <bananenvla1234@gmail.com>
2024-03-14 14:08:18 +0100

    CSS en PHP/HTML overhaul

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SuperCoaster NV</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Welkop op SuperCoaster NV</h1>
+    </header>
+
+    <section>
+        <h2>About Us</h2>
+        <p>SuperCoaster NV is een bedrijf dat achtbanen ontwikkelt en bouwt. We werken momenteel aan een nieuw type achtbaan. Binnenkort zullen er meer details beschikbaar zijn.
+.</p>
+    </section>
+
+    <section>
+        <h2>Onze nieuwe achtbaan</h2>
+        <p>Meer details zullen snel verschijnen!</p>
+        <!-- Placeholder voor images -->
+        <img src="https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png">
+    </section>
+
+    <section>
+        <h2>Contact ons</h2>
+        <form action="contact.php" method="post">
+            <label for="name">Naam:</label><br>
+            <input type="text" id="name" name="name"><br>
+            <label for="email">Email:</label><br>
+            <input type="text" id="email" name="email"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </section>
+</body>
+</html>
